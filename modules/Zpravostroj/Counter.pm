98087c51ca223135824f9d326f79e9b952d22672
Karel Bilek <bilek7am@u-pl26.ms.mff.cuni.cz>
2009-09-21 05:58:41 +0200

    just moved modules around to make them work

---
+++ modules/Zpravostroj/Counter.pm
@@ -0,0 +1,175 @@
+package Zpravostroj::Counter;
+use 5.008;
+use strict;
+use warnings;
+
+use Zpravostroj::Other;
+use TectoMT::Document;
+use TectoMT::Bundle;
+
+use Encode;
+
+
+
+use base 'Exporter';
+our @EXPORT = qw( countTMT);
+
+my $max_length=10;
+	#max. length of THEME
+	#longer = more memory
+
+my $max_first_entities = 15;
+my $max_first_themes = 15;
+	#number of entities/themes that get it to final keys
+	#BUT! this is BEFORE the subthemes cleaning!
+	
+sub clean_subthemes {
+	
+	my $hashref = shift;
+
+	foreach my $theme (keys %$hashref) {
+			#keys are generated in the beginning, so it has to test existence again
+		if ($hashref->{$theme}) {
+			foreach(allSubThemes(" ",$theme)) {
+				delete $hashref->{$_};
+			}
+
+		}
+	}
+}
+
+sub just_first{
+	#take just first N stuff, based on score
+	my $n = shift;
+	my $hashref = shift;
+	my $it = 0;
+	foreach my $key (sort {$hashref->{$b}<=>$hashref->{$a}} keys %{$hashref}) {
+		$it++;
+		if ($it > $n) {
+			delete ($hashref->{$key});
+		}
+	}
+}
+
+sub count_TMT {
+
+	
+	my %named_entities;
+
+	my %themes;
+		#hashes with scores of entites
+		#lemma=>score
+		
+	my %forms;
+		#hash, used to retrieve form from lemma
+		
+	my @last_words;
+
+
+	my $unused_forms="";
+		#forms, that had not been used
+	
+	my $text = shift;
+	my $document = TectoMT::Document->new;
+	$document->set_attr("czech_source_text", $text);
+
+	sub add {
+		my $hashref = shift;
+		my @normal_words = @_;
+		
+		while (@normal_words) {
+	        #every subset ending on the "right" end of normal_words
+	
+			my $joined = join (" ", @normal_words);
+			
+			my $length;
+			if ($forms{$joined}){
+				$length = split_size($forms{$joined});
+				$length = 1 unless $length;
+			} else {
+				$length = @normal_words;
+			}
+
+	        ($hashref->{$joined})+=2-(1/$length);
+
+	        shift @normal_words;
+	    }
+
+	}
+	
+	sub remember_forms {
+	        #will remember all original forms, based on the global variables
+	    my @last_words_copy = @last_words;
+	    while (@last_words_copy) {
+	        my $joined = join(" ", map($_->[0], @last_words_copy));
+	        $forms{$joined} = join("_", map($_->[1], @last_words_copy)) unless defined $forms{$joined};
+	        shift @last_words_copy
+	    }
+	}
+
+	sub count_node {
+		my $node = shift;
+			
+		if ($node->get_deref_attr('m.rf')) {
+			#it is a named entity.
+			add(\%named_entities, $name) if 
+					((my $type=($node->get_attr('ne_type'))) 
+				and 
+					(length(my $name = $node->get_attr('normalized_name'))>3) 
+				and 
+					($type =~ /^(g|m|q|P|ps)/));
+					
+		} elsif (my $lemma = ($node->get_attr('lemma'))) {
+			#it is a word
+			
+			if (my $lemma_b = (make_normal_word($lemma))) { 
+				#the one that I like
+				my $form = ($node->get_attr('form'));
+
+				push (@last_words, [$lemma_b, $lost_forms.$form]);
+				$lost_forms = "";
+						
+				if (@last_words > $max_length) {
+					shift @last_words;
+				}
+				
+				remember_forms;
+				add(\%themes, map($_->[0], @last_words));
+			} else {
+				if (is_word($node->get_attr('form'))) {
+					#"a", "nebo", "který" ...
+					$lostforms=$lost_forms.$node->get_attr('form')." ";		
+				}
+			}
+		}
+	    if (my @children = ($node->get_children)) {
+	        map (coun_node($_), @children);
+	    }
+	}
+	
+	foreach my $bundle ( $document->get_bundles() ) {
+		
+		Report::fatal("Evaluated bundle does not have SCzechN tree") unless ($bundle->contains_tree('SCzechN'));
+		Report::fatal("Evaluated bundle does not have SCzechM tree") unless ($bundle->contains_tree('SCzechM'));
+		my $n_root = $bundle->get_tree('SCzechN');
+		my $m_root = $bundle->get_tree('SCzechM');
+		
+		count_node($m_root);
+		count_node($m_root);
+	
+	}
+	
+
+	
+	just_first($max_first_entities, \%named_entities);
+	just_first($max_first_themes, \%themes);
+    
+
+	my %superhash = (%themes, %named_entities);
+	clean_subthemes(\%superhash);
+	
+	my @res = map (($_."|".($forms{$_}?($forms{$_}):($_))), keys %superhash);
+    return join ("\n", @res);
+}
+
+1;
---
+++ modules/Zpravostroj/Other.pm
@@ -0,0 +1,82 @@
+package Zpravostroj::Other;
+
+use YAML::XS qw(LoadFile);
+
+use strict;
+use FindBin;
+use warnings;
+
+use utf8;
+use Encode;
+
+use base 'Exporter';
+our @EXPORT = qw(split_size all_subthemes is_word is_banned make_normal_word);
+
+#workaround for weird split behaviour in scalar context
+sub split_size{my $r=shift;my @ol=split (" ", $r);return scalar @ol;}
+
+sub utf8_array_to_hash {
+	my @array = @_;
+	my %hash = map ((decode("utf8", $_)=>1), @{$arrayref});
+	return %hash;
+}
+
+sub all_subthemes {
+	my $delimit=shift;
+	my @res;
+	
+	foreach my $theme (@_) {
+		my @themes = split($delimit, $theme);
+		while (@themes) {
+			my @themes_c = @themes;
+			while (@themes_c) {
+				my $joined = join($delimit, @themes_c);
+				push(@res, $joined) unless ($joined eq $theme);
+				pop @themes_c;
+			}
+			shift @themes;
+		}
+	}
+	return @res;
+}
+
+my $banned_read=0;
+my %banned=();
+my $czechs="ÁČĎĚÉÍŇÓŘŠŤÚŮÝŽáčďěéíňóřšťúůýž";
+
+sub load_yaml_file {
+	my $name = shift;
+	my $ref = LoadFile("$FindBin::Bin/../$name");
+	return $ref;
+}
+
+sub is_word {
+	my $what=shift;
+	return ($what =~ /^[A-Za-z0-9$czechs]+$/);
+}
+
+sub is_banned{
+	if (!$banned_read) {
+		%banned=utf8_array_to_hash(@{load_yaml_file("information/banned_words.yaml")});
+		$banned_read=1;
+	}
+	my $what=shift;
+	return ($banned{$what});
+}
+
+sub make_normal_word {
+
+    my $text = shift;
+    return unless defined $text;
+    
+    $text =~ s/^([A-Za-z$czechs ]*).*$/$1/;
+        #remove all weird letters
+    
+    $text =~ s/ *$//;
+        #remove final space(s)
+	
+    return $text if (((length ($text))>=3) and !($banned{$text}));
+	"";
+}
+
+1;
