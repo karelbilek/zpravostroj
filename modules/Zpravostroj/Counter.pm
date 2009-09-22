675f6df64fab7aa1b931ab4b008621926183f435
Karel Bilek <bilek7am@u-pl26.ms.mff.cuni.cz>
2009-09-22 15:28:38 +0200

    changed to normal subroutines again (still doesnt work)

--- modules/Zpravostroj/Counter.pm
+++ modules/Zpravostroj/Counter.pm
@@ -26,34 +26,42 @@ my $max_first_themes = 15;
 sub clean_subthemes {
 	
 	my $hashref = shift;
+	my %themes = %$hashref;
 
-	foreach my $theme (keys %$hashref) {
+	foreach my $theme (keys %themes) {
 			#keys are generated in the beginning, so it has to test existence again
-		if ($hashref->{$theme}) {
+		if ($themes{$theme}) {
 			foreach(allSubThemes(" ",$theme)) {
-				delete $hashref->{$_};
+				delete $themes{$_};
 			}
 
 		}
 	}
 }
 
-sub just_first{
+sub just_first {
 	#take just first N stuff, based on score
-	my $n = shift;
 	my $hashref = shift;
+	my %themes =%$hashref;
+	
+	my $n = shift;
+	
 	my $it = 0;
-	foreach my $key (sort {$hashref->{$b}<=>$hashref->{$a}} keys %{$hashref}) {
+	foreach my $key (sort {$themes{$b}<=>$themes{$a}} keys %themes) {
 		$it++;
 		if ($it > $n) {
-			delete ($hashref->{$key});
+			delete ($themes{$key});
 		}
 	}
 }
 
 sub add {
 	my $formsref = shift;
-	my $hashref = shift;
+	my %forms = %$formsref;
+	
+	my $themeref = shift;
+	my %themes = %$themeref;
+	
 	my @normal_words = @_;
 	
 	while (@normal_words) {
@@ -63,17 +71,72 @@ sub add {
 		
 		my $length;
 		if ($forms{$joined}){
+				#!!!!! pozor pozor!! achtung bitte!
+				# the length of the WHOLE FORM - with all those "lost forms" before - gets counted
+				# but it gets counted to the LEMMA version in %themes hash!
 			$length = split_size($forms{$joined});
 			$length = 1 unless $length;
 		} else {
 			$length = @normal_words;
 		}
-
-        ($hashref->{$joined})+=2-(1/$length);
+				#here - $joined is LEMMA!!
+        ($themes{$joined})+=2-(1/$length);
 
         shift @normal_words;
     }
 
+}
+
+sub count_node {
+	my $named_entitiesref = shift;
+	my %named_entities = %$named_entitiesref;
+	
+	my $themesref = shift;
+	my %themes = %$themesref;
+	
+	my $formsref = shift;
+	my %forms = %$formsref;
+	
+	my $last_wordsref = shift;
+	my @last_words = @$last_wordsref;
+	
+	my $unused_formsref = shift;
+	my $unused_forms = $$unused_formsref;
+	
+	my $node = shift;
+		
+	if ($node->get_deref_attr('m.rf')) {
+		#it is a named entity.
+		my $type;
+		if (($type=($node->get_attr('ne_type'))) and (length(my $name = $node->get_attr('normalized_name'))>3) and ($type =~ /^(g|m|q|P|ps)/)) {
+			$add(\%forms, \%named_entities, $name); 
+		}
+	} elsif (my $lemma = ($node->get_attr('lemma'))) {
+		#it is a word
+		
+		if (my $lemma_b = (make_normal_word($lemma))) { 
+			#the one that I like
+			my $form = ($node->get_attr('form'));
+
+			push (@last_words, {"lemma" => $lemma_b, "form" => $unused_forms.$form});
+			$unused_forms = "";
+					
+			if (@last_words > $max_length) {
+				shift @last_words;
+			}
+			
+			&$remember_forms;
+			&$add(\%themes, map($_->[0], @last_words));
+		} else {
+			if (is_word($node->get_attr('form'))) {
+				#"a", "nebo", "který" ...
+				$unused_forms=$unused_forms.$node->get_attr('form')." ";		
+			}
+		}
+	}
+    if (my @children = ($node->get_children)) {
+        map ($count_node(\%named_entities, \%themes, \%forms, \@last_words, \$unused_forms, $_), @children);
+    }
 }
 
 sub count_TMT {
@@ -97,77 +160,6 @@ sub count_TMT {
 	my $text = shift;
 	my $document = TectoMT::Document->new;
 	$document->set_attr("czech_source_text", $text);
-
-	my $add = sub {
-		my $hashref = shift;
-		my @normal_words = @_;
-		
-		while (@normal_words) {
-	        #every subset ending on the "right" end of normal_words
-	
-			my $joined = join (" ", @normal_words);
-			
-			my $length;
-			if ($forms{$joined}){
-				$length = split_size($forms{$joined});
-				$length = 1 unless $length;
-			} else {
-				$length = @normal_words;
-			}
-
-	        ($hashref->{$joined})+=2-(1/$length);
-
-	        shift @normal_words;
-	    }
-
-	};
-	
-	my $remember_forms = sub {
-	        #will remember all original forms, based on the global variables
-	    my @last_words_copy = @last_words;
-	    while (@last_words_copy) {
-	        my $joined = join(" ", map($_->[0], @last_words_copy));
-	        $forms{$joined} = join("_", map($_->[1], @last_words_copy)) unless defined $forms{$joined};
-	        shift @last_words_copy
-	    }
-	};
-
-	my $count_node = sub {
-		my $node = shift;
-			
-		if ($node->get_deref_attr('m.rf')) {
-			#it is a named entity.
-			my $type;
-			if (($type=($node->get_attr('ne_type'))) and (length(my $name = $node->get_attr('normalized_name'))>3) and ($type =~ /^(g|m|q|P|ps)/)) {
-				&$add(\%named_entities, $name); 
-			}
-		} elsif (my $lemma = ($node->get_attr('lemma'))) {
-			#it is a word
-			
-			if (my $lemma_b = (make_normal_word($lemma))) { 
-				#the one that I like
-				my $form = ($node->get_attr('form'));
-
-				push (@last_words, [$lemma_b, $unused_forms.$form]);
-				$unused_forms = "";
-						
-				if (@last_words > $max_length) {
-					shift @last_words;
-				}
-				
-				&$remember_forms;
-				&$add(\%themes, map($_->[0], @last_words));
-			} else {
-				if (is_word($node->get_attr('form'))) {
-					#"a", "nebo", "který" ...
-					$unused_forms=$unused_forms.$node->get_attr('form')." ";		
-				}
-			}
-		}
-	    if (my @children = ($node->get_children)) {
-	        map (&$count_node($_), @children);
-	    }
-	}
 	
 	foreach my $bundle ( $document->get_bundles() ) {
 		
@@ -176,15 +168,13 @@ sub count_TMT {
 		my $n_root = $bundle->get_tree('SCzechN');
 		my $m_root = $bundle->get_tree('SCzechM');
 		
-		&$count_node($m_root);
-		&$count_node($m_root);
+		&$count_node(\%named_entities, \%themes, \%forms, \@last_words, \$unused_forms, $n_root);
+		&$count_node(\%named_entities, \%themes, \%forms, \@last_words, \$unused_forms, $m_root);
 	
 	}
-	
-
-	
-	just_first($max_first_entities, \%named_entities);
-	just_first($max_first_themes, \%themes);
+		
+	just_first(\%named_entities, $max_first_entities);
+	just_first(\%themes, $max_first_themes);
     
 
 	my %superhash = (%themes, %named_entities);
