15dfa2831944b07d86de903b0a4d349506f787b5
Karel Bilek <bilek7am@u-pl26.ms.mff.cuni.cz>
2009-09-24 00:00:49 +0200

    added moar stuff to Database

--- modules/Zpravostroj/Database.pm
+++ modules/Zpravostroj/Database.pm
@@ -6,6 +6,8 @@ use warnings;
 use File::Slurp;
 use File::Touch;
 
+use YAML::XS qw(LoadFile DumpFile);
+
 use Zpravostroj::Other;
 
 
@@ -13,7 +15,8 @@ my $database_dir = read_option("data_address")."/articles";
 my $pool_dir = $database_dir."/pool";
 my $count_file = $pool_dir."/count";
 
-my @all_article_options = @{read_option("all_article_options")};
+my %all_article_properties;
+@all_article_properties{@{read_option("all_article_properties")}}=();
 
 sub get_pool_count {
 	if (-d $pool_address) {
@@ -31,5 +34,24 @@ sub get_pool_count {
 }
 
 sub add_new_articles {
-	
+	my @articles = @_;
+	my $i = get_pool_count;
+	foreach my $article_ref (@articles) {
+		foreach (keys %$article_ref) {
+			(exists $all_article_properties{$_}) or die "forbidden article property $_";
+		}
+		DumpFile($pooldir."/".$i, $article_ref);
+		$i++;
+	}
+	write_file($count_file, $i);
+}
+
+sub read_articles {
+	my $begin = shift;
+	$begin=0 if !$begin;
+	my @result;
+	foreach my $i ($begin..(get_pool_count-1)){
+		push (@result, LoadFile($pooldir."/".$i));
+	}
+	return @result;
 }
