c531e22dd34b62d1ecfe54769bd107a741fa78f6
Karel Bilek <bilek7am@u-pl26.ms.mff.cuni.cz>
2009-09-30 04:08:03 +0200

    so, deamon now, MAYBE, really works!

--- scripts_experimental/daemon.pl
+++ scripts_experimental/daemon.pl
@@ -4,8 +4,13 @@ use strict;
 
 use Zpravostroj::Walker;
 
+use Zpravostroj::Other;
+my $script = glob(read_option("password_script_dir")."/*.sh");
+my $pass = `$script`;
+
 while (1) {
 	step;
-	
+	`klog -pa $pass`;	#this is sadly needed :(
 	sleep(18000);
 }
+
