404cf27d2ed26031902053cdfff6194044825957
Karel Bilek <bilek7am@u-pl21.ms.mff.cuni.cz>
2009-10-13 03:01:01 +0200

    simple fixes

--- scripts_experimental/clean_data.sh
+++ scripts_experimental/clean_data.sh
@@ -1,4 +1,5 @@
 rm -r /afs/ms/u/b/bilek7am/BIG/skola/rocnik/data/articles/pool/
 rm /afs/ms/u/b/bilek7am/BIG/skola/rocnik/data/RSS/*
 rm /afs/ms/u/b/bilek7am/BIG/skola/rocnik/data/log
+rm /afs/ms/u/b/bilek7am/BIG/skola/rocnik/data/warnings
 rm ./nohup.out
--- scripts_experimental/test_counter.pl
+++ scripts_experimental/test_counter.pl
@@ -2,8 +2,8 @@ use YAML::XS;
 use Zpravostroj::Database;
 use Zpravostroj::Counter;
 
-$what = 41;
-($article)=read_articles($what, $what);
+$what = 35;
+($article)=read_pool_articles($what, $what);
 ($article)=count_themes($article);
 
 #print Dump $article->{keys};
