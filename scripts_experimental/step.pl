d9ef20088fc20b781f00a1ba6833133813abd637
Karel Bilek <bilek@ufallab2.ms.mff.cuni.cz>
2009-11-15 23:11:42 +0100

    cleaning up

---
+++ scripts_experimental/step.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+use strict;
+
+use Zpravostroj::Walker;
+
+use Zpravostroj::Other;
+#my $script = glob(read_option("password_script_dir")."/*.sh");
+#my $pass = `$script`;
+
+#while (1) {
+	step;
+#	sleep(14400);
+#}
+
