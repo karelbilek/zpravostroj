de4471122614e310f8840f064b41ff66af183ead
Karel Bilek <bilek@ufallab2.ms.mff.cuni.cz>
2009-12-27 01:03:17 +0100

    I will probably kill myself now.

--- modules/Zpravostroj/Other.pm
+++ modules/Zpravostroj/Other.pm
@@ -55,7 +55,7 @@ sub my_warning {
 	
 	my $day_dir = $warning_dir."/".get_day();
 	
-	if (-d $day_dir) {mkdir $day_dir or die "cannot mkdir $day_dir\n";}
+	if (!-d $day_dir) {mkdir $day_dir or die "cannot mkdir $day_dir\n";}
 	
 	my $warning_file = $day_dir."/".$type;
 	
